ba5eef97b90d84672f6dc8f71d024bbf3a9f149d
Pablo-Quintanilla <janpablomj@gmail.com>
2020-07-06 03:55:25 -0500

    APINuestra

--- app.js
+++ app.js
@@ -6,14 +6,37 @@ const configMensaje = require('./configMensaje');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.post('/formulario', (req, res) => {
     configMensaje(req.body);
     res.status(200).send();
    });
 
-app.get('/home',(req,res) => {
-    res.send({message: "Hola Mundo"});
-})
+
+//API para calcular el IMC de la persona
+app.post('/imc', (req, res) => {
+    //Obtenemos los valores del formulario
+    console.log(req.body);
+    const { sexo, estatura, peso } = req.body;  //con esto hacemos desestructuracion
+    //Hacemos el calculo del IMC y si está en sobrepeso o no
+    let imc = peso/(estatura*estatura);
+    let nivel = ""
+    if(imc > 30){
+        nivel = "Obesidad";
+    } else if(imc > 25){
+        nivel = "Sobrepeso";
+    } else if(imc > 18.5){
+        nivel = "Normal";
+    } else {
+        nivel = "Bajo Peso"
+    }
+    res.status(200).send({
+        imc: imc,
+        nivel: nivel
+    });
+});
+
 app.listen(3000, () => {
 console.log('Servidor corriendo')
 });
