c99bb50c2479a480907ed99c5fd9ecfad3173f63
Daniela Gutierrez <you@example.com>
2020-12-09 22:39:48 -0600

    base datos

--- app.js
+++ app.js
@@ -2,16 +2,48 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const configMensaje = require('./configMensaje');
+var mysql = require('mysql');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}));
 
+var conexion= mysql.createConnection({
+    host : 'localhost',
+    database : 'redes',
+    user : 'root',
+    password : 'dany',
+});
+
+conexion.connect(function(err) {
+    if (err) {
+        console.error('Error de conexion: ' + err.stack);
+        return;
+    }
+    console.log('Conectado con el identificador ' + conexion.threadId);
+});
+
+
 app.post('/formulario', (req, res) => {
     configMensaje(req.body);
     res.status(200).send();
    });
+
+app.get('/getCursos', (req,res) => {
+    var data = new Array();
+    conexion.query('SELECT * FROM cursos', function (error, results, fields) {
+        if (error)
+            throw error;
+        results.forEach(result => {
+            req = results;
+            console.log(result);
+        });
+        res.status(200).json({
+            data: results
+        })
+    });
+});
 
 
 //API para calcular el IMC de la persona
@@ -36,6 +68,8 @@ app.post('/imc', (req, res) => {
         nivel: nivel
     });
 });
+
+
 
 app.listen(3000, () => {
 console.log('Servidor corriendo')
